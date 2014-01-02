ecf7bc90f9ade7bc691771b5a6fa6397d5d24bbd
Simon Deng <simonsdeng@gmail.com>
2014-01-01 22:55:56 -0800

    Set up SPA structure

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var container;
+
+// load content through AJAX
+function load(url, callback) {
+	var xhr = new XMLHttpRequest();
+	
+	xhr.onreadystatechange = function () {
+		callback(this.responseText);
+	}
+	
+	xhr.open("get", url, true);
+	xhr.send();
+}
+
+// update page content
+function show(content) {
+	container.innerHTML = content;
+}
+
+// init on phonegap ready
+function init() {
+	container = document.getElementById("container");
+	
+	load("main.html", show);
+}
+
+document.addEventListener("deviceready", init, false);
